8f4e2374f24cc5ec7d263759f30f12b4264d1d7b
rabindranathfv <rabindranathucv@gmail.com>
2019-05-15 16:14:43 -0400

    add ngrx store, ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // StoreModule.forRoot({ count: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 100, // retains last 100 states
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
