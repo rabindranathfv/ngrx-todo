9b428baa0930f1133902014e8c93428a4eacb1be
rabindranathfv <rabindranathucv@gmail.com>
2019-09-25 19:08:48 -0300

    add filters into todo-footer component with new filter actions

--- src/app/todo-components/todo-footer/todo-footer.component.ts
+++ src/app/todo-components/todo-footer/todo-footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as fromFilters from '../../filter/filter.actions';
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
   styleUrls: ['./todo-footer.component.scss']
 })
 export class TodoFooterComponent implements OnInit {
+
+  public filtersValid: fromFilters.filterValid [] = ['todos', 'completados', 'pendientes'];
 
   constructor() { }
 
